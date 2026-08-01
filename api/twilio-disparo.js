0d0027fca9accc63175e1ccbe1f488ceb6aba869
veracitytech26 <rodrigomonteiro1912@gmail.com>
2026-07-31 22:50:32 -0300

    Update twilio-disparo.js

--- api/twilio-disparo.js
+++ api/twilio-disparo.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
 
       // Retorna todos os templates que têm aprovação WhatsApp (qualquer status)
       const templates = contents
-        .filter(t => t)
+        .filter(t => t.friendly_name)
         .map(t => ({
           sid: t.sid,
           nome: t.friendly_name,
